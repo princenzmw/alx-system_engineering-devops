6a4f98e2d984c4825e310865e963e8e1f624fdfc
princenzmw <princenzmw@gmail.com>
2024-03-27 19:11:12 +0200

    trying to fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,24 +1,6 @@
-# A puppet manifest that install flask from pip3.
-exec { 'update':
-  command => '/usr/bin/apt-get update'
-}
-
-package { 'python3-pip':
-  ensure  => installed,
-  require => Exec['update']
-}
-
-exec { 'uninstall werkzeug':
-  command => '/usr/bin/pip3 uninstall -y werkzeug',
-  require => Package['python3-pip']
-}
-
-exec { 'install werkzeug':
-  command => '/usr/bin/pip3 install werkzeug==0.16.1',
-  require => Exec['uninstall werkzeug']
-}
-
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  require => Exec['install werkzeug']
+#!/usr/bin/pup
+# A puppet manifest to install a flask(2.1.0) using package (pip3)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
