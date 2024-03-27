4f419e2ce35833129173065070027cb93dec7cd4
princenzmw <princenzmw@gmail.com>
2024-03-27 11:24:27 +0200

    trying to solve the checker

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# A puppet manifest that install flask from pip3
+exec { 'update':
+  command => '/usr/bin/apt-get update'
+}
+
+package { 'python3-pip':
+  ensure   => installed,
+  require  => Exec['update']
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  require => Package['python3-pip']
 }
